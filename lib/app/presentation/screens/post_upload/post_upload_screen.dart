4792165bba41bff70e66a13428b29ddf18b63c81
drunisa007 <newarunkatwal@gmail.com>
2022-06-29 17:33:51 +0630

    Solve The Container Taking Full Height

--- lib/app/presentation/screens/post_upload/post_upload_screen.dart
+++ lib/app/presentation/screens/post_upload/post_upload_screen.dart
@@ -24,64 +24,65 @@ class PostUploadScreen extends StatelessWidget {
         title: const Text('Upload your new post.'),
         centerTitle: true,
       ),
-      body: Container(
-        width: double.infinity,
-        height: 380.h,
-        padding: EdgeInsets.symmetric(
-          vertical: 10.w,
-          horizontal: 30.w,
-        ),
-        child: PostSingleBg(
-            customChild: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.asset(
-                "assets/images/post.jpeg",
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: 185.h,
-              ),
-            ),
-            SizedBox(
-              height: 8.h,
-            ),
-            TextField(
-              textAlign: TextAlign.left,
-              controller: mPostTextController,
-              maxLines: 2,
-              autofocus: true,
-              decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'What is on your mind?',
-                  hintStyle: TextStyle(color: Colors.grey)),
-              style: const TextStyle(
-                color: Colors.black,
-              ),
-            ),
-            SizedBox(
-              height: 8.h,
-            ),
-            Obx((){
-              return ElevatedButton(
-                onPressed: () {
-                  if(mPostUploadController.postText.value.isNotEmpty){
-           
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: mPostUploadController.postText.value.isEmpty?Colors.grey:Theme.of(context).primaryColor,
-                  onPrimary: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 10.w,
+            horizontal: 30.w,
+          ),
+          child: PostSingleBg(
+              customChild: Column(
+                mainAxisSize: MainAxisSize.min,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.asset(
+                  "assets/images/post.jpeg",
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 185.h,
                 ),
-                child: const Center(
-                  child: Text('Upload'),
-                ));
-            })
-          ],
-        )),
+              ),
+              SizedBox(
+                height: 8.h,
+              ),
+              TextField(
+                textAlign: TextAlign.left,
+                controller: mPostTextController,
+                maxLines: 2,
+                autofocus: true,
+                decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'What is on your mind?',
+                    hintStyle: TextStyle(color: Colors.grey)),
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: 8.h,
+              ),
+              Obx((){
+                return ElevatedButton(
+                  onPressed: () {
+                    if(mPostUploadController.postText.value.isNotEmpty){
+             
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: mPostUploadController.postText.value.isEmpty?Colors.grey:Theme.of(context).primaryColor,
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text('Upload'),
+                  ));
+              })
+            ],
+          )),
+        ),
       ),
     );
   }
