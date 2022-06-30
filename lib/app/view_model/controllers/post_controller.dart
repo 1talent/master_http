678f9c8e90c8ef8eac1b311f2034f8193979c8d3
drunisa007 <newarunkatwal@gmail.com>
2022-06-30 17:08:27 +0630

    Calling FetchPost List in Controller

--- lib/app/view_model/controllers/post_controller.dart
+++ lib/app/view_model/controllers/post_controller.dart
@@ -2,23 +2,31 @@ import 'dart:convert';
 
 import 'package:get/get.dart';
 import 'package:tuto_http/app/service/app_data/data/constant_post.dart';
+import 'package:tuto_http/app/service/services/post_service.dart';
 import 'package:tuto_http/app/service/vos/post_vo.dart';
 
 class PostController extends GetxController {
-  PostController() {
-    getPostList();
+
+  late PostService service = PostService();
+  
+  RxList<PostVo> postList = RxList();
+
+  PostController(){
     fetchPostList();
   }
-
-  RxList<PostVo> postList = RxList();
 
   void getPostList() {
     List<PostVo> mPostList = ConstantPost().getAllPost();
     postList.addAll(mPostList);
   }
 
-  fetchPostList()  {
-  
+  fetchPostList() async {
+   var resultPostList = await service.getPostList();
+
+   if(resultPostList!=null){
+    postList.addAll(resultPostList);
+   }
+   
   }
 
 
